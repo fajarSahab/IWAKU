068518669cf63746d819ed545bee21527cf0bf06
fajar sahab <114895919+fajarSahab@users.noreply.github.com>
2023-11-27 20:12:50 +0700

    perbaikan daftar memberlist dan payment

--- resources/views/Payment/daftarPayment.blade.php
+++ resources/views/Payment/daftarPayment.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IWAKU App</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
             margin: 0;
@@ -108,16 +109,50 @@
 
     <div class="navbar">
         <div class="app-name">IWAKU</div>
+
         <div class="top-right-menu">
+            <div class="icon">
+
+            </div>
             <div class="icon">
                 <i class="fas fa-search"></i>
             </div>
             <div class="icon">
                 <i class="fas fa-bell"></i>
             </div>
-            <div class="icon">
-                <i class="fas fa-user"></i>
-            </div>
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button
+                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <div><i class="fas fa-user"></i></i></div>
+
+                        <div class="ms-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <x-dropdown-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
+                    </x-dropdown-link>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
+                </x-slot>
+            </x-dropdown>
         </div>
     </div>
 
